c7081c1c39df1a8f69f55741110d1f4e48547f77
Saarthak Gupta <saarthakgupta08@gmail.com>
2020-07-15 00:50:35 +0530

    add quick_sort

--- Sorting_Searching/quick_sort_median.cpp
+++ Sorting_Searching/quick_sort_median.cpp
@@ -4,6 +4,7 @@ using namespace std;
 
 int* quick_sort(int* arr, int l, int r);
 int partition(int* a, int l, int r);
+int get_median(int *a,int l,int r);
 
 int main() {
 	int n;
@@ -31,24 +32,47 @@ int* quick_sort(int* arr, int l, int r) {
 	return arr;
 }
 
+
 int partition(int* a, int l, int r) {
-	int pivot_element=a[l];
 	int i=l,j=r;
+	int pivot_index=get_median(a,l,r);
+	int temp=a[l];
+	a[l]=a[pivot_index];
+	a[pivot_index]=temp;
+	int pivot=a[l];
 	while(i<j) {
-		while(i<=r&&a[i]<=pivot_element) {
+		while(i<=r&&a[i]<=pivot) {
 			i++;
 		}
-		while(j>0&&a[j]>pivot_element) {
+		while(j>0&&a[j]>pivot) {
 			j--;
 		}
 		if(i<j) {
-			int temp=a[i];
+			int temp2=a[i];
 			a[i]=a[j];
-			a[j]=temp;
+			a[j]=temp2;
 		}
 	}
-	int temp=a[j];
-	a[j]=pivot_element;
-	a[l]=temp;
+	int temp3=a[j];
+	a[j]=pivot;
+	a[l]=temp3;
 	return j;
+}
+
+
+int get_median(int *a,int l,int z) {
+	int c=(l+z)/2;
+	int q=a[c];
+	int p=a[l];
+	int r=a[z];
+	if((p>=q&&p<r)||(p>q&&p<=r)||(p>=r&&p<q)||(p>r||p<=q)) {
+		return l;
+	}
+	else if((q>=p&&q<r)||(q>p&&q<=r)||(q>=r&&q<p)||(q>r||q<=p)) {
+		return c;
+	}
+	else {
+		return r;
+	}
+
 }
