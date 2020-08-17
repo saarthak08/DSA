8b59897512385b443eafadfa540dc0468b21ab00
Saarthak Gupta <saarthakgupta08@gmail.com>
2020-08-17 08:52:14 +0530

    add LeetCode submissions

---
+++ Competitive/LeetCode/ReverseLinkedList.java
@@ -0,0 +1,34 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode reverseList(ListNode head) {
+        ListNode temp=head;
+        if(head==null) {
+            return null;
+        }
+        while(temp.next!=null) {
+            temp=temp.next;
+        }
+        reverse(head);
+        return temp;
+    }
+    
+    public ListNode reverse(ListNode node) {
+        if(node!=null) {
+            ListNode node1=reverse(node.next);
+            if(node1!=null) {
+                node1.next=node;
+                node.next=null;
+            } 
+        }
+        return node;
+    }
+}
---
+++ Competitive/LeetCode/StringToInteger.java
@@ -0,0 +1,58 @@
+class Solution {
+    public int myAtoi(String s) {
+        int i=0;
+        long num=0;
+        boolean neg=false;
+        boolean sign = false;
+        boolean space = false;
+        while(i<s.length()){
+            if(s.charAt(i)=='-' && num==0 && !sign){
+                sign=true;
+                neg=true;
+                space=true;
+                i++;
+                continue;
+            } else if(s.charAt(i)=='+' && num==0 && !sign){
+                sign=true;
+                neg=false;
+                space=true;
+                i++;
+                continue;
+            }
+            if(s.charAt(i) == ' ' && !space){
+                i++;
+                continue;
+            }
+            
+            if(!space)space = true;
+            if(!sign)sign = true;
+            if(!isNumeric(s.charAt(i))) break;
+            num*=10;
+            num+=convertNumeric(s.charAt(i));
+            
+            
+            if(num>Integer.MAX_VALUE || num<Integer.MIN_VALUE){
+               break;
+            }
+            
+            i++;
+        }
+        
+        if(neg) num*=-1;
+        if(num>Integer.MAX_VALUE){
+            return Integer.MAX_VALUE;
+        }
+        else if(num<Integer.MIN_VALUE) {
+            return Integer.MIN_VALUE;
+        }
+        return (int)num;
+    }
+    
+    public boolean isNumeric(char c){
+        return (c>='0' && c<='9');
+    }
+    public int convertNumeric(char c){
+        //0-9
+        return (int)c- '0';
+    }
+}
