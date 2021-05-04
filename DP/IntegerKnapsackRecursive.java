4d8382f29de7714e85c210cfc25c0636348bf3da
Saarthak Gupta <saarthakgupta08@gmail.com>
2021-05-05 01:45:37 +0530

    add more DP codes

---
+++ DP/IntegerKnapsackRecursive.java
@@ -0,0 +1,36 @@
+import java.util.*;
+
+public class IntegerKnapsackRecursive {
+     public static Scanner sc = new Scanner(System.in);
+
+    public static void main(String[] args) {
+       int n;
+       n=sc.nextInt();
+       int[] wt = new int[n];
+       int[] v = new int[n];
+       int C = sc.nextInt();
+       for(int i=0;i<n;i++) {
+       		wt[i]=sc.nextInt();
+       }
+       for(int i=0;i<n;i++) {
+       		v[i]=sc.nextInt();
+       }
+       int[] dp=new int[n];
+       Arrays.fill(dp,-1);
+       System.out.println(maxCap(n,C,wt,v,dp,0));
+        
+    
+}
+
+	public static int maxCap(int n, int C, int[] wt, int[] v, int[] dp,int i) {	
+
+		if(i>=n||wt[i]>C) {
+			return 0;
+		}
+		if(dp[i]!=-1) {
+			return dp[i];
+		}
+		dp[i]=Math.max(v[i]+maxCap(n,C-wt[i],wt,v,dp,i),maxCap(n,C,wt,v,dp,i+1));
+		return dp[i];
+	}
+}
---
+++ DP/ZeroOneKnapsackRecursive.java
@@ -0,0 +1,45 @@
+import java.util.*;
+
+public class ZeroOneKnapsackRecursive {
+
+	public static Scanner sc=new Scanner(System.in);
+
+	public static void main(String[] args) {
+		int n;
+		n=sc.nextInt();
+		int wt[]=new int[n];
+		int v[]=new int[n];
+		int C;
+		C=sc.nextInt();
+		for(int i=0;i<n;i++) {
+			wt[i]=sc.nextInt();
+		}
+		for(int i=0;i<n;i++) {
+			v[i]=sc.nextInt();
+		}
+		int dp[][] = new int[n+1][C+1];
+		for(int i=0;i<n+1;i++) {
+			for(int j=0;j<C+1;j++) {
+				if(i==0||j==0) {
+					dp[i][j]=0;
+				} else {
+					dp[i][j]=-1;
+				}
+			}
+		}
+		
+		System.out.println(maxCap(n,C,wt,v,dp,1));
+
+	}
+
+	public static int maxCap(int n, int C, int wt[], int v[], int dp[][],int i) {
+		if(i>n||wt[i-1]>C) {
+			return 0;
+		}
+		if(dp[i][C]!=-1) {
+			return dp[i][C];
+		}
+		dp[i][C]=Math.max(v[i-1]+maxCap(n,C-wt[i-1],wt,v,dp,i+1),maxCap(n,C,wt,v,dp,i+1));
+		return dp[i][C];
+	}
+}
